7bba1a1d0ddc2fd53864e35a1f01289bf9d6fcec
TudoHuang <88125758+tudohuang@users.noreply.github.com>
2024-04-28 17:32:15 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SrtGen',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['whisper', 'pydub', 'numpy', 'opencc', 'torch', 're'],
+    entry_points={
+        'console_scripts': [
+            'srt-gen = srtgen:main'
+        ]
+    }
+)
