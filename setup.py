1b841463c707f20b852ba8cb012bf1ad8dc3c7bd
TudoHuang <88125758+tudohuang@users.noreply.github.com>
2024-04-28 17:32:53 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='SrtGen',
     version='0.1',
     packages=find_packages(),
-    install_requires=['whisper', 'pydub', 'numpy', 'opencc', 'torch', 're'],
+    install_requires=['whisper', 'pydub', 'numpy', 'opencc', 'torch'],
     entry_points={
         'console_scripts': [
             'srt-gen = srtgen:main'
